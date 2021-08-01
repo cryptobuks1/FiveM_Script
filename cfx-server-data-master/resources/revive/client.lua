47f9e2a2af19b6d0620a3d4dd78eacae1bfd6f33
aekkawatt <62130201+aekkawatt@users.noreply.github.com>
2021-08-01 18:46:16 +0700

    edit revive

--- cfx-server-data-master/resources/revive/client.lua
+++ cfx-server-data-master/resources/revive/client.lua
@@ -1,9 +1,7 @@
 --------------------------------
 --- RP Revive, Made by FAXES ---
 --------------------------------
-
 --- Config ---
-
 local reviveWait = 10 -- Change the amount of time to wait before allowing revive (in seconds).
 local featureColor = "~y~" -- Game color used as the button key colors.
 
@@ -19,89 +17,83 @@ end)
 
 RegisterNetEvent("RPRevive:CheckPermission:Return")
 AddEventHandler("RPRevive:CheckPermission:Return", function(havePerms)
-	cHavePerms = havePerms
+    cHavePerms = havePerms
 end)
 
 -- Turn off automatic respawn here instead of updating FiveM file.
 AddEventHandler('onClientMapStart', function()
-	Citizen.Trace("RPRevive: Disabling the autospawn.")
-	exports.spawnmanager:spawnPlayer() -- Ensure player spawns into server.
-	Citizen.Wait(2500)
-	exports.spawnmanager:setAutoSpawn(false)
-	Citizen.Trace("RPRevive: Autospawn is disabled.")
+    Citizen.Trace("RPRevive: Disabling the autospawn.")
+    exports.spawnmanager:spawnPlayer() -- Ensure player spawns into server.
+    Citizen.Wait(2500)
+    exports.spawnmanager:setAutoSpawn(false)
+    Citizen.Trace("RPRevive: Autospawn is disabled.")
 end)
 
 function respawnPed(ped, coords)
-	SetEntityCoordsNoOffset(ped, coords.x, coords.y, coords.z, false, false, false, true)
-	NetworkResurrectLocalPlayer(coords.x, coords.y, coords.z, coords.heading, true, false) 
-	SetPlayerInvincible(ped, false) 
-	TriggerEvent('playerSpawned', coords.x, coords.y, coords.z, coords.heading)
-	ClearPedBloodDamage(ped)
+    SetEntityCoordsNoOffset(ped, coords.x, coords.y, coords.z, false, false, false, true)
+    NetworkResurrectLocalPlayer(coords.x, coords.y, coords.z, coords.heading, true, false)
+    SetPlayerInvincible(ped, false)
+    -- TriggerEvent('playerSpawned', coords.x, coords.y, coords.z, coords.heading)
+    ClearPedBloodDamage(ped)
 end
 
 function revivePed(ped)
-	local playerPos = GetEntityCoords(ped, true)
-	isDead = false
-	timerCount = reviveWait
-	NetworkResurrectLocalPlayer(playerPos, true, true, false)
-	SetPlayerInvincible(ped, false)
-	ClearPedBloodDamage(ped)
+    local playerPos = GetEntityCoords(ped, true)
+    isDead = false
+    timerCount = reviveWait
+    NetworkResurrectLocalPlayer(playerPos, true, true, false)
+    SetPlayerInvincible(ped, false)
+    ClearPedBloodDamage(ped)
 end
 
 function ShowInfoRevive(text)
-	SetNotificationTextEntry("STRING")
-	AddTextComponentSubstringPlayerName(text)
-	DrawNotification(true, true)
+    SetNotificationTextEntry("STRING")
+    AddTextComponentSubstringPlayerName(text)
+    DrawNotification(true, true)
 end
 
 Citizen.CreateThread(function()
-	local respawnCount = 0
-	local spawnPoints = {}
-	local playerIndex = NetworkGetPlayerIndex(-1) or 0
-	math.randomseed(playerIndex)
+    local respawnCount = 0
+    local spawnPoints = {}
+    local playerIndex = NetworkGetPlayerIndex(-1) or 0
+    math.randomseed(playerIndex)
 
-	function createSpawnPoint(x, y, z, heading)
-		-- local xValue = math.random(x1,x2) + 0.0001
-		-- local yValue = math.random(y1,y2) + 0.0001
-
-		local newObject = {
-			x = x,
-			y = y,
-			z = z + 0.0001,
-			heading = heading + 0.0001
-		}
-		table.insert(spawnPoints,newObject)
-	end
-
-	-- createSpawnPoint(-448, -448, -340, -329, 35.5, 0) -- Mount Zonah
-	createSpawnPoint(250, -570, 43.3, 0)   -- Pillbox Hill
-	-- createSpawnPoint(335, 340, -1400, -1390, 34.0, 0) -- Central Los Santos
-	-- createSpawnPoint(1850, 1854, 3700, 3704, 35.0, 0) -- Sandy Shores
-	createSpawnPoint(-247, 6328, 33.5, 0) -- Paleto
+    local positions = { -- spawn position
+    {
+        x = 298.45,
+        y = -585.20,
+        z = 43.26,
+        h = 90.2
+    }, -- your spawn position
+    {
+        x = -247.55,
+        y = 6330.77,
+        z = 32.43,
+        h = 252.67
+    }}
 
     while true do
-    Citizen.Wait(0)
-		ped = GetPlayerPed(-1)
+        Citizen.Wait(0)
+        local ped = GetPlayerPed(-1)
         if IsEntityDead(ped) then
-			isDead = true
-			print('IsEntityDead')
+            isDead = true
             SetPlayerInvincible(ped, true)
             SetEntityHealth(ped, 1)
-			ShowInfoRevive('You are dead. Use ~y~E ~w~to revive or ~y~R ~w~to respawn.')
+            ShowInfoRevive('You are dead. Use ~y~E ~w~to revive or ~y~R ~w~to respawn.')
             if IsControlJustReleased(0, 38) and GetLastInputMethod(0) then
                 if timerCount <= 0 or cHavePerms then
                     revivePed(ped)
-				else
-					TriggerEvent('chat:addMessage', {args = {'^*Wait ' .. timerCount .. ' more seconds before reviving.'}})
-                end	
-            elseif IsControlJustReleased(0, 45) and GetLastInputMethod( 0 ) then
-                local coords = spawnPoints[math.random(1,#spawnPoints)]
-				print('IsEntityDead')
-				respawnPed(ped, coords)
-				isDead = false
-				timerCount = reviveWait
-				respawnCount = respawnCount + 1
-				math.randomseed(playerIndex * respawnCount)
+                else
+                    TriggerEvent('chat:addMessage', {
+                        args = {'^*Wait ' .. timerCount .. ' more seconds before reviving.'}
+                    })
+                end
+            elseif IsControlJustReleased(0, 45) and GetLastInputMethod(0) then
+                respawnPed(ped, positions[math.random(1, #positions)])
+                isDead = false
+                timerCount = reviveWait
+                respawnCount = respawnCount + 1
+                math.randomseed(playerIndex * respawnCount)
             end
         end
     end
@@ -110,8 +102,8 @@ end)
 Citizen.CreateThread(function()
     while true do
         if isDead then
-			timerCount = timerCount - 1
+            timerCount = timerCount - 1
         end
-        Citizen.Wait(1000)          
+        Citizen.Wait(1000)
     end
 end)
