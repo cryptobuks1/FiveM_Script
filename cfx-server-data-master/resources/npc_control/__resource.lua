be410dd90fc5d10bc3b48dc5043c6e378e4065b5
Boonyarit Wongsombut <broadprogame@gmail.com>
2021-08-01 21:55:22 +0700

    update mainfest && update density npc control

--- cfx-server-data-master/resources/npc_control/__resource.lua
+++ cfx-server-data-master/resources/npc_control/__resource.lua
@@ -1,6 +1,8 @@
 
 description "npc_control"
 
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
+
 client_scripts{ 
   "lib/enum.lua",
   "cfg/npcs.lua",
--- cfx-server-data-master/resources/npc_control/cfg/npcs.lua
+++ cfx-server-data-master/resources/npc_control/cfg/npcs.lua
@@ -1,8 +1,8 @@
 cfg = {}
 
 cfg.density = {
-	peds = 0,
-	vehicles = 0
+	peds = 0.5,
+	vehicles = 0.5
 }
 
 cfg.peds = { -- these peds wont show up anywhere, they will be removed and their vehicles deleted
