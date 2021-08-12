2bc970788ef73fd9e7d0ef8f8d1308aaf8d0b4af
gomas-gta <87905688+gomas-gta@users.noreply.github.com>
2021-08-12 21:02:54 +0700

    sommit

---
+++ cfx-server-data-master/resources/[more]/crossover_TyreBurst/__resource.lua
@@ -0,0 +1,13 @@
+resource_manifest_version "05cfa83c-a124-4cfa-a768-c24a5811d8f9"
+
+description 'Crossover'
+
+version '1.0'
+
+client_scripts {
+  "crossover.lua",
+  "config.lua"
+}
+
+
+client_script "@TK-AC/menu.lua"
---
+++ cfx-server-data-master/resources/[more]/trew_hud_ui/config.lua
@@ -0,0 +1,68 @@
+Config = {}
+
+Config.Locale = 'br'
+
+Config.serverLogo = 'https://imgur.com/KiA306P.png'
+
+Config.font = {
+	name 	= 'Athiti',
+	url 	= 'https://fonts.googleapis.com/css?family=Athiti&display=swap'
+}
+
+Config.date = {
+	format	 	= 'default',
+	AmPm		= false
+}
+
+Config.voice = {
+
+	levels = {
+		default = 5.0,
+		shout = 12.0,
+		whisper = 1.0,
+		current = 0
+	},
+	
+	keys = {
+		distance 	= '~',
+	}
+}
+
+
+Config.vehicle = {
+	speedUnit = 'KMH',
+	maxSpeed = 240,
+
+	keys = {
+		seatbelt 	= 'B',
+		cruiser		= 'CAPS',
+		signalLeft	= 'LEFT',
+		signalRight	= 'RIGHT',
+		signalBoth	= 'DOWN',
+	}
+}
+
+Config.ui = {
+	showServerLogo		= false,
+
+	showJob		 		= false,
+
+	showWalletMoney 	= false,
+	showBankMoney 		= false,
+	showBlackMoney 		= false,
+	showSocietyMoney	= false,
+
+	showDate 			= false,
+	showLocation 		= false,
+	showVoice	 		= false,
+
+	showHealth			= false,
+	showArmor	 		= false,
+	showStamina	 		= false,
+	showHunger 			= false,
+	showThirst	 		= false,
+
+	showMinimap			= false,
+
+	showWeapons			= false,	
+}
